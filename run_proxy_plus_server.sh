b0b635c9b235d5622edde69869dd0eac8c7ce92c
drankincms <dylansr90@gmail.com>
2020-07-01 10:30:42 -0400

    Update run_proxy_plus_server.sh

--- run_proxy_plus_server.sh
+++ run_proxy_plus_server.sh
@@ -2,7 +2,7 @@
 
 set -m
 
-../server build_dir.hw.xilinx_u250_xdma_201830_2/alveo_hls4ml.xclbin 8083 8 &
+../server ./aws_hls4ml.awsxclbin 8083 8 &
 ./haproxy-2.0.14/haproxy -f haproxy.cfg >& srv.log
 
 fg %1
